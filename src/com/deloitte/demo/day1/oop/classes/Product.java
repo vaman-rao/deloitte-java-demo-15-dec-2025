91ab15928172b076bb26950c517b461c6a20b598
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-15 12:59:40 +0530

    class and object

--- src/com/deloitte/demo/day1/oop/classes/Product.java
+++ src/com/deloitte/demo/day1/oop/classes/Product.java
@@ -1,5 +1,16 @@
 package com.deloitte.demo.day1.oop.classes;
 
 public class Product {
+	
+//	fields and methods 
+	int id;
+	String name;
+	double price;
+	
+	void sale() {
+		System.out.println(name + " is priced at " + price + ".");
+	}
+	
+	
 
 }
