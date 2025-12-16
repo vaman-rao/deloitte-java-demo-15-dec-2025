086f14eaf1fd38cb57e676ec31b96de8f71c8599
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-16 12:13:01 +0530

    object methods

--- src/com/deloitte/demo/day2/oop/Employee.java
+++ src/com/deloitte/demo/day2/oop/Employee.java
@@ -1,5 +1,7 @@
 package com.deloitte.demo.day2.oop;
 
+import java.util.Objects;
+
 public class Employee {
 
 	private int id;
@@ -41,9 +43,29 @@ public class Employee {
 		this.salary = salary;
 	}
 	
+	
+	
 //	hashCode 
 	
 //	equals 
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, salary);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return id == other.id && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(salary) == Double.doubleToLongBits(other.salary);
+	}
 
 	@Override
 	public String toString() {
