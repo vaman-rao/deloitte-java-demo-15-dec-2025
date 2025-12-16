8eb92660c5e68bdcbcc18e94f0ac916466d8c965
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-16 11:50:30 +0530

    encapsulation

--- src/com/deloitte/demo/day2/oop/Employee.java
+++ src/com/deloitte/demo/day2/oop/Employee.java
@@ -6,6 +6,17 @@ public class Employee {
 	private String name;
 	private double salary;
 
+	public Employee() {
+		super();
+	}
+
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -28,6 +39,11 @@ public class Employee {
 
 	public void setSalary(double salary) {
 		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
 	}
 
 }
