8e59683bfb92c7997f297dba11d4d925f361a644
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-18 15:18:11 +0530

    stream api

--- src/com/deloitte/demo/day4/steramsapi/StreamsDemo.java
+++ src/com/deloitte/demo/day4/steramsapi/StreamsDemo.java
@@ -3,6 +3,7 @@ package com.deloitte.demo.day4.steramsapi;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
 
@@ -14,7 +15,30 @@ public class StreamsDemo {
 		System.out.println("All employees:");
 		empList.forEach(e -> System.out.println(e.toString()));
 
-		System.out.println("Employees with salary > 92K:");
+//		System.out.println("Employees with salary > 92K using for each loop:");
+//		for (Employee emp : empList) {
+//			if (emp.getSalary() > 92000)
+//				System.out.println(emp.toString());
+//		}
+
+		System.out.println("Employees with salary > 92K using Streams API:");
+
+//		// convert list to stream
+		Stream<Employee> empStream = empList.stream();
+//		// perform required operation
+//		Stream<Employee> empWithHighSalary = empStream.filter(e -> e.getSalary() > 92000);
+//		// process the result
+//		empWithHighSalary.forEach(e -> System.out.println(e.toString()));
+		// shortcut syntax -
+//		empList.stream().filter(e -> e.getSalary() > 92000).forEach(e -> System.out.println(e.toString()));
+//		// other methods 
+//		empStream.map(null);
+//		empStream.limit(0);
+//		empStream.skip(0);
+
+		List<Employee> empListWithHighSal = empList.stream().filter(e -> e.getSalary() > 92000).toList();
+
+		empListWithHighSal.forEach(e -> System.out.println(e));
 
 	}
 
