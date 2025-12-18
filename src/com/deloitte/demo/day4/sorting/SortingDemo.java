f25998f8efda33319ebd4f115c326a4fcb35dc83
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-18 16:30:09 +0530

    stream api

--- src/com/deloitte/demo/day4/sorting/SortingDemo.java
+++ src/com/deloitte/demo/day4/sorting/SortingDemo.java
@@ -3,6 +3,7 @@ package com.deloitte.demo.day4.sorting;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 // Comparator, Comparable 
@@ -21,15 +22,32 @@ public class SortingDemo {
 		friendList.forEach(f -> System.out.println(f));
 
 		List<Employee> empList = new ArrayList<Employee>(Arrays.asList(new Employee(101, "Sonu", 90000),
-				new Employee(102, "Monu", 95000), new Employee(103, "Tonu", 93000), new Employee(104, "Sonu", 92000)));
+				new Employee(102, "Monu", 95000), new Employee(103, "Tonu", 93000), new Employee(104, "Ponu", 92000)));
 
 		System.out.println("EmployeeList in insertion order:");
 		empList.forEach(e -> System.out.println(e));
 
 		System.out.println("EmployeeList in the order of name:");
-		Collections.sort(empList);
+
+//		Collections.sort(empList);
+//		empList.forEach(e -> System.out.println(e));
+//		Collections.sort(empList, Comparator.comparing(e -> e.getName().reversed()));
+//		Collections.sort(empList, Comparator.comparing(e -> e.getName()).thenComparing(e -> e.getSalary()));
+
+		// Sort by name in descending order (reversed), then by salary in ascending
+		// order
+		Collections.sort(empList,
+				Comparator.comparing((Employee e) -> e.getName()).reversed().thenComparing(e -> e.getSalary()));
+
+		// Sort by name in ascending order, then by salary in descending order
+		Collections.sort(empList,
+				Comparator.comparing((Employee e) -> e.getName()).thenComparing(e -> e.getSalary()).reversed());
+
 		empList.forEach(e -> System.out.println(e));
 
+		System.out.println("EmployeeList in the order of salary:");
+		Collections.sort(empList, Comparator.comparing(e -> e.getSalary()));
+		empList.forEach(e -> System.out.println(e));
 	}
 
 }
