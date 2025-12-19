486a84afcb51ee301c9a9939cb0f12dcd489795b
vaman-rao <contact.vaman.rao@gmail.com>
2025-12-19 15:43:13 +0530

    jdbc

--- src/com/deloitte/demo/day5/jdbc/JdbcDemo.java
+++ src/com/deloitte/demo/day5/jdbc/JdbcDemo.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 // download jar from here - 
 // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j/8.2.0
@@ -14,6 +15,8 @@ public class JdbcDemo {
 	public static void main(String[] args) throws SQLException {
 
 		System.out.println("Start");
+		JdbcDemo.getData();
+		JdbcDemo.addData();
 		JdbcDemo.getData();
 		System.out.println("End");
 
@@ -25,18 +28,45 @@ public class JdbcDemo {
 				sql = "SELECT id, name, salary FROM employees";
 
 		Connection con = DriverManager.getConnection(url, user, password);
-		System.out.println(con);
 
 		PreparedStatement ps = con.prepareStatement(sql);
-		System.out.println(ps);
 
 		ResultSet rs = ps.executeQuery();
-		System.out.println(rs);
 
 		while (rs.next())
 			System.out.println(rs.getInt("id") + " " + rs.getString("name") + " " + rs.getDouble("salary"));
-
 	}
+
+	static void addData() throws SQLException {
+
+		String url = "jdbc:mysql://localhost:3306/deloitte", user = "root", password = "root",
+				sql = "INSERT INTO employees (id, name, salary) VALUES (?, ?, ?)";
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter id: ");
+		int id = sc.nextInt();
+
+		System.out.print("Enter name: ");
+		String name = sc.next();
+
+		System.out.print("Enter salary: ");
+		double salary = sc.nextDouble();
+
+		Connection con = DriverManager.getConnection(url, user, password);
+		PreparedStatement ps = con.prepareStatement(sql);
+
+		ps.setInt(1, id);
+		ps.setString(2, name);
+		ps.setDouble(3, salary);
+
+		int rows = ps.executeUpdate();
+
+		sc.close();
+
+		System.out.println(rows + " record inserted successfully.");
+	}
+
 }
 
 ////public class JdbcDemo {
